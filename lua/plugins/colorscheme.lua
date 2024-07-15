49d4dd38734326177a0f4517e57d699663f1eb54
Jorge Cohen <me@jorgecohen.codes>
2024-07-15 08:57:22 +0100

    configs

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,48 @@
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+  { "echasnovski/mini.map", version = "*" },
+  {
+    "akinsho/toggleterm.nvim",
+    config = function()
+      require("toggleterm").setup({
+        open_mapping = [[<c-\>]],
+        shade_terminals = false,
+        -- add --login so ~/.zprofile is loaded
+        -- https://vi.stackexchange.com/questions/16019/neovim-terminal-not-reading-bash-profile/16021#16021
+      })
+    end,
+    keys = {
+      { [[<C-\>]] },
+      { "<leader>0", "<Cmd>2ToggleTerm<Cr>", desc = "Terminal #2" },
+      {
+        "<leader>td",
+        "<cmd>ToggleTerm size=40 dir=~/ direction=horizontal<cr>",
+        desc = "Open a horizontal terminal at the Desktop directory",
+      },
+      {
+        "<leader>tv",
+        function()
+          local count = vim.v.count1
+          require("toggleterm").toggle(count, 0, vim.loop.cwd(), "vertical")
+        end,
+        desc = "ToggleTerm (vertical)",
+      },
+      {
+        "<leader>th",
+        function()
+          local count = vim.v.count1
+          require("toggleterm").toggle(count, 10, vim.loop.cwd(), "horizontal")
+        end,
+        desc = "ToggleTerm (horizontal)",
+      },
+    },
+  },
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
